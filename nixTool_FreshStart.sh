d89989211ce1678eb0a8bcb225f4d8dabac709a7
James Gallier <jmkgallier@gmail.com>
2020-03-07 15:04:25 -0500

    Renamed file, fixed typos and indents

--- nixTool_FreshStart.sh
+++ nixTool_FreshStart.sh
@@ -12,7 +12,7 @@ system_Refresh () {
   apt -qq autoremove -y
 }
 
-# Prepare directories to be used by CURRENT script
+# Prepare directories to be used by script
 prep_ScriptSetupDirectory () {
   mkdir "${USER_HOME}"/Downloads/Sandbox/
   mkdir "${USER_HOME}"/.local/bin/
@@ -38,11 +38,13 @@ install_JBToolbox () {
 # Apply Audio sink to fix Microphone-Speaker Echo
 fix_PulseAudioEcho () {
   echo 'load-module module-echo-cancel source_name=logitechsource' >> /etc/pulse/default.pa
-echo 'set-default-source logitechsource' >> /etc/pulse/default.pa
+  echo 'set-default-source logitechsource' >> /etc/pulse/default.pa
 }
 
 # Apply fix for screen tearing on systems with intel-gpu
-fix_InterScreenTear () {
+# This requires a check for dir&file in question
+# This requires a check textstring echoed into file
+fix_IntelScreenTear () {
   mkdir -p /etc/X11/xorg.conf.d/
   echo 'Section "Device"
      Identifier  "Intel Graphics"
@@ -56,8 +58,8 @@ system_Refresh
 prep_ScriptSetupDirectory # Check permissions on created directories
 
 # Requested Package Section
-install_JBToolbox
-prep_VBox
+install_JBToolbox # Update links/Stop relying on links
+prep_VBox # Update links/Stop relying on links
 
 # Mass Package Installation
 apt -qq update && sleep 3
@@ -65,8 +67,10 @@ apt install wget snapd steam-installer neofetch -y
 apt install chromium-browser nmap deluge htop arc-theme -y
 apt install exfat-fuse exfat-utils python3-distutils python3-pip libavcodec-extra -y
 apt install virtualbox-6.1 virtualbox-guest-x11 virtualbox-guest-utils virtualbox-guest-dkms -y
-#apt install psensor
+
 #apt install gnome-tweak-tool gnome-shell-extensions chrome-gnome-shell
+
+
 
 ## Install Snap Packages
 snap install spotify
@@ -74,7 +78,7 @@ snap install atom --classic
 
 # Hotfixes
 fix_PulseAudioEcho # Requires check
-fix_InterScreenTear # Requires conditional to check for intel system
+fix_IntelScreenTear # Requires conditional to check for intel system
 
 # Wrap up Installation
 
@@ -90,17 +94,19 @@ clear && neofetch
 ## Docker, PIA(include installer w/script), wine + winetricks + arduino + Xsane + Bethesda Launcher + Rockstar Games + Arduino IDE
 ## Antivirus/Rootkit, Linux Security Fixes
 ## Add Bash expects for Y/n and keep maintainer pkg
-## Add bash variables for github accounts creation
 
 ### Experimental implementations
 
-## Prepare BalenaEtcher Installation
+## - Prepare BalenaEtcher Installation -
 #echo "deb https://deb.etcher.io stable etcher" | tee /etc/apt/sources.list.d/balena-etcher.list
 #apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 #apt -qq update
 #apt install balena-etcher-electron
+# - -
 
-## Install Temp/Package/Compatibility software
+## - Install Temp/Package/Compatibility software -
 #apt install lm-sensors hddtemp -y
 #sensors-detect
 #sensors
+#apt install psensor
+# - -
