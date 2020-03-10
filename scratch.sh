ca5a438222187f251a865f25eab9761f630dd5c5
James Gallier <jmkgallier@gmail.com>
2020-03-09 21:29:55 -0400

    optimized gitconfig function yes/no handler

--- scratch.sh
+++ scratch.sh
@@ -6,55 +6,14 @@
 #USER_CURRENT_DE=#env | grep XDG_CURRENT_DESKTOP | cut -d '=' -f 2-
 #LOCAL_KERN_VERSION=#uname -v
 
-# Install Targets
-# User Variables
+# Install Targets !#
+# User Variables !#
 
-# "Is correct?"
-input_checkResp () {
-  # $1 is initial conditional statement to be answered by this function.
-  local conditional_resp
-  read -rp "${1}" conditional_resp
-  if [ "${conditional_resp,}" == "n" ]; then
-    echo 0
-  elif [ "${conditional_resp,}" == "y" ]; then
-    echo 1
-  else
-    input_checkResp "$1"
-  fi
-}
-
-# Git Config function
-gitConfig () {
-  user_confirm=0
-  user_condit="Is the above Git Config Correct? [Y/n]"
-
-  printf "Hello, %s\nPlease enter a username and email address to document your Git commits.\n" "${USER}"
-  read -rp "Username: " var1
-  read -rp "Email Address: " var2
-  printf "\n"
-  git config --global user.name "${var1}"
-  git config --global user.email "${var2}"
-  git config --list | grep user
-
-  while [ "${user_confirm}" == 0 ]; do
-    user_resp=$(input_checkResp "${user_condit}") ##figure out assigning value of func
-    user_confirm=$user_resp
-
-    if [ "${user_confirm}" == 0 ]; then
-      gitConfig
-    elif [ "${user_confirm}" == 1 ]; then
-      echo "Git Identity Saved (Globally)"
-    else
-      echo "Failure at gitConfig()>input_checkResp()"
-      exit 1;
-    fi
-  done
-}
-
-## Future Improvements:
-## Docker, PIA(include installer w/script), wine + winetricks + arduino + Xsane + Bethesda Launcher + Rockstar Games + Arduino IDE
+#### Future Improvements:
+## Docker, PIA(include installer w/script), wine + winetricks +
+## arduino + Xsane + Bethesda Launcher + Rockstar Games + Arduino IDE
 ## Antivirus/Rootkit, Linux Security Fixes
-## Add Bash expects for Y/n and keep maintainer pkg
+## Make Python helper Class to handle GNU->BSD compatibility issues
 
 ### Experimental implementations
 
