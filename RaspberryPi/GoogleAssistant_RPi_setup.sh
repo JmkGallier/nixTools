93b90248486e1a1183dbba0eb56fea9768c9e16c
James Gallier <jmkgallier@gmail.com>
2020-04-22 14:48:36 -0400

    consolidated pip installs

--- RaspberryPi/GoogleAssistant_RPi_setup.sh
+++ RaspberryPi/GoogleAssistant_RPi_setup.sh
@@ -32,8 +32,7 @@ install_GA() {
   python3 -m venv env
   env/bin/python3 -m pip install --upgrade pip setuptools wheel
   source env/bin/activate
-  python3 -m pip install --upgrade google-assistant-sdk[samples]
-  python3 -m pip install --upgrade google-auth-oauthlib[tool]
+  python3 -m pip install --upgrade google-assistant-sdk[samples] google-auth-oauthlib[tool]
   google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless --client-secrets "${CLIENT_SECRET}"
 }
 
@@ -54,7 +53,7 @@ config_Speaker() {
   pcm.speaker {
     type plug
     slave {
-    pcm 'hw:${SPEAKER_address}'
+      pcm 'hw:${SPEAKER_address}'
       rate 16000
     }
   }" >> ~/.asoundrc
